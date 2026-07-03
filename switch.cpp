55d278a58d3ff97018d02aa02a96b68266ef8174
bigfaraday <welsinhort@gmail.com>
2026-07-03 10:25:49 -0400

    ejemplo de los meses comentado en switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -4,53 +4,53 @@
 
 int main()
 {
+    /*
+        int mes;
+        std::cout << "ingrese un numero del 1 al 12: ";
+        std::cin >> mes;
 
-    int mes;
-    std::cout << "ingrese un numero del 1 al 12: ";
-    std::cin >> mes;
-
-    switch (mes) // se pone dentro del parentesis la variable a evaluar y la condicion se pone a lado de los case
-    {
-    case 1: // esto es equivalente a decir "si mes es igual a 1 entonces imprime es enero"
-        std::cout << "es enero";
-        break;
-    case 2:
-        std::cout << "es febrero";
-        break;
-    case 3:
-        std::cout << "es marzo";
-        break;
-    case 4:
-        std::cout << "es abril";
-        break;
-    case 5:
-        std::cout << "es mayo";
-        break;
-    case 6:
-        std::cout << "es junio";
-        break;
-    case 7:
-        std::cout << "es julio";
-        break;
-    case 8:
-        std::cout << "es agosto";
-        break;
-    case 9:
-        std::cout << "es septiembre";
-        break;
-    case 10:
-        std::cout << "es octubre";
-        break;
-    case 11:
-        std::cout << "es noviembre";
-        break;
-    case 12:
-        std::cout << "es diciembre";
-        break;
-    default: // default es el equivalente al else, si no se cumple ningun case entonces se ejecuta lo que esta en default
-        std::cout << "solo ingrese un numero del 1 al 12";
-    }
-
+        switch (mes) // se pone dentro del parentesis la variable a evaluar y la condicion se pone a lado de los case
+        {
+        case 1: // esto es equivalente a decir "si mes es igual a 1 entonces imprime es enero"
+            std::cout << "es enero";
+            break;
+        case 2:
+            std::cout << "es febrero";
+            break;
+        case 3:
+            std::cout << "es marzo";
+            break;
+        case 4:
+            std::cout << "es abril";
+            break;
+        case 5:
+            std::cout << "es mayo";
+            break;
+        case 6:
+            std::cout << "es junio";
+            break;
+        case 7:
+            std::cout << "es julio";
+            break;
+        case 8:
+            std::cout << "es agosto";
+            break;
+        case 9:
+            std::cout << "es septiembre";
+            break;
+        case 10:
+            std::cout << "es octubre";
+            break;
+        case 11:
+            std::cout << "es noviembre";
+            break;
+        case 12:
+            std::cout << "es diciembre";
+            break;
+        default: // default es el equivalente al else, si no se cumple ningun case entonces se ejecuta lo que esta en default
+            std::cout << "solo ingrese un numero del 1 al 12";
+        }
+    */
     // ejemplo 2
     char calificacion;
 
